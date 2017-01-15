00eb56795c7020406c8be6c4966f7fac6317a146
Benjamin Staffin <bstaffin@fitbit.com>
2017-01-14 17:35:34 -0800

    mostly working checkpoint

---
+++ Knit.hs
@@ -0,0 +1,67 @@
+-- Authors:
+--   Mike MacHenry    https://github.com/mmachenry
+--   Benjamin Staffin https://github.com/benley
+
+import KnitParse
+
+-- For the right side
+cmd2Str cmd = case cmd of
+  K -> "|"
+  K2Tog -> "/"
+  K3Tog -> "/"
+  P -> "-"
+  P2Tog -> "/"
+  Slppsso -> "/"
+  Slkpsso -> "\\"
+  Ssk -> "\\"
+  Yo -> "O"
+
+runKnit :: [Row] -> [[Command]] -> [String]
+runKnit [] result = map (concatMap cmd2Str) result
+runKnit (row:rows) result = runKnit rows (knitRow row : result)
+
+knitRow row = case row of
+  WrongSide _ cmds -> map wrongSideTrans cmds
+  RightSide _ cmds -> reverse cmds
+
+wrongSideTrans cmd = case cmd of
+  K -> P
+  P -> K
+  a -> a
+
+test :: [Row]
+test = [
+  WrongSide 1 (replicate 121 K),
+  RightSide 2 (replicate 121 P),
+  WrongSide 3 (replicate 4 K ++ replicate 113 P ++ replicate 4 K),
+  RightSide 4 (
+    (replicate 4 P)
+    ++ (replicate 12 K)
+    ++ [Ssk, Yo, K,K,K, Ssk, Yo, K,K,K, Ssk, Yo, K,K,K,K,K, Ssk, Yo]
+    ++ (replicate 8 K)
+    ++ [Ssk, Yo, K,K,K, Ssk, Yo]
+    ++ (replicate 7 K)
+    ++ [Yo, K2Tog]
+    ++ (replicate 6 K)
+    ++ [Ssk, Yo]
+    ++ (replicate 10 K)
+    ++ [Ssk, Yo, K,K,K, Ssk, Yo, K,K, Yo, Slppsso, Yo, K,K,K, Ssk, Yo]
+    ++ (replicate 9 K)
+    ++ [Yo, K2Tog, K,K,K,K, Ssk, Yo, K,K,K, Ssk, Yo, K, P,P,P,P])
+  ]
+
+main = do
+  f <- readFile "new.txt"
+  let l = lines f
+  let rows = map readRow l
+  let output = runKnit rows []
+  mapM_ putStrLn output
+
+{-
+file = [
+  "Row 1 (ws): k121",
+  "Row 2 (rs): p121",
+  "Row 3 (ws): k4, p113, k4",
+  "Row 4 (rs): p4, k12, ssk, yo, k3, ssk, yo, k3, ssk, yo, k5, ssk, yo, k8,ssk, yo, k3, ssk, yo, k7, yo, k2 tog, k6, ssk, yo, k10, ssk, yo, k3, ssk, yo, k2, yo, sl 1-k2 tog-psso, yo, k3, ssk, yo, k9, yo, k2 tog, k4, ssk, yo, k3, ssk, yo, k1, p4"
+  ]
+-}
---
+++ KnitParse.hs
@@ -0,0 +1,122 @@
+module KnitParse where
+
+import Control.Monad (void)
+import Text.Megaparsec
+import Text.Megaparsec.String
+import qualified Text.Megaparsec.Lexer as L
+
+data Row = WrongSide Int [Command]
+         | RightSide Int [Command]
+         deriving Show
+
+data Command =
+    K
+    | K2Tog
+    | K3Tog
+    | P
+    | P2Tog
+    | Slppsso
+    | Slkpsso
+    | Ssk
+    | Yo
+    deriving Show
+
+sc :: Parser ()
+sc = L.space (void (char ' ')) lineComment blockComment where
+  lineComment = L.skipLineComment "//" <|> L.skipLineComment "#"
+  blockComment = L.skipBlockComment "/*" "*/"
+
+lexeme :: Parser a -> Parser a
+lexeme = L.lexeme sc
+
+knit :: Parser [Command]
+knit = try knit3 <|> try knit2 <|> do
+    char 'k'
+    n <- read <$> lexeme (some digitChar)
+    return (replicate n K)
+
+knit2 :: Parser [Command]
+knit2 = lexeme (string "k2") >> lexeme (string "tog") >> return [K2Tog]
+
+knit3 :: Parser [Command]
+knit3 = lexeme (string "k3") >> lexeme (string "tog") >> return [K3Tog]
+
+purl :: Parser [Command]
+purl = try purl2 <|> do
+    char 'p'
+    n <- read <$> lexeme (some digitChar)
+    return (replicate n P)
+
+ktoken x = lexeme (string x)
+
+purl2 :: Parser [Command]
+purl2 = ktoken "p2" >> ktoken "tog" >> return [P2Tog]
+
+sl1 :: Parser [Command]
+sl1 = do
+    ktoken "sl"
+    choice [ ktoken "1-k2" >> ktoken "tog-psso"  >> return [Slkpsso]
+           , ktoken "1" >> ktoken "wyif-p1-psso" >> return [Slppsso]
+           ]
+
+parenExpr :: Parser [Command]
+parenExpr = inParens (concat <$> parseExpr `sepBy` ktoken ",")
+
+twiceExpr :: Parser [Command]
+twiceExpr = (concat . replicate 2) <$> (parenExpr <* ktoken "twice")
+
+parseExpr :: Parser [Command]
+parseExpr = choice [ starsExpr
+                   , twiceExpr
+                   , ktoken "yo"  >> return [Yo]
+                   , ktoken "ssk" >> return [Ssk]
+                   , sl1
+                   , purl
+                   , knit]
+
+parseMany :: Parser [Command]
+parseMany = concat <$> (parseExpr `sepBy` ktoken ",")
+
+parseManyAndStars :: Parser [Command]
+parseManyAndStars = concat <$> some (parseMany <|> starsExpr)
+
+inParens :: Parser a -> Parser a
+inParens p = ktoken "(" *> p <* ktoken ")"
+
+starsExpr :: Parser [Command]
+starsExpr = do
+    ktoken "*"
+    cmds <- parseMany
+    ktoken "*"
+    repN <- read <$> lexeme (some digitChar)
+    ktoken "x"
+    return (concat $ replicate repN cmds)
+
+
+parseLine :: Parser Row
+parseLine = do
+    ktoken "Row"
+    rowN <- read <$> lexeme (some digitChar)
+    side <- inParens (ktoken "rs" <|> ktoken "ws")
+    ktoken ":"
+    cmds <- parseMany
+    return $ case side of
+      "rs" -> RightSide rowN cmds
+      "ws" -> WrongSide rowN cmds
+
+parseFile = parseLine `sepBy` char '\n'
+
+readRow :: String -> Row
+readRow input =
+    case runParser parseLine "knitting" input of
+      Right v -> v
+      Left err -> error (show err)
+
+readRows :: String -> [Row]
+readRows input = let inlines = lines input in
+  map readRow inlines
+
+justparse :: String -> [Row]
+justparse input = case runParser (many parseLine) "knitting" input of
+                    Right v -> v
+                    Left err -> error (show err)
